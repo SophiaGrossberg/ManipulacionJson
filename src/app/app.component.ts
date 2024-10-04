1f4147bd752e01f2c8b87677128d60a0bf275dac
Sophia Grossberg <sophia@Sophias-Mac-mini.local>
2024-10-03 20:07:58 -0500

    Subir proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from './Models/Usuario.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ManipulacionJson';
+
+export class AppComponent implements OnInit {
+
+  usuarios: Usuario[] = []; 
+  nuevousuario: Usuario = { nombre: '', email: '', empresa: '' };  // Inicializar el objeto Usuario
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.obtenerUsuario();
+
+  }
+
+obtenerUsuario(){
+  this.http.get<any[]>('https://jsonplaceholder.typicode.com/users').subscribe(data => {
+    this.usuarios= data.map(user => ({
+
+      nombre: user.name,
+      email: user.email,
+      empresa: user.company.name
+
+    }));
+  });
 }
+
+agregarUsuario(){
+
+  const body = {
+    name: this.nuevousuario.nombre,
+    email: this.nuevousuario.email,
+    company: {
+      name:this.nuevousuario.empresa
+    }
+  }
+
+this.http.post('https://jsonplaceholder.typicode.com/users',body).subscribe(response => {
+
+  console.log('Usuario agregado',response);
+  this.usuarios.push(this.nuevousuario);
+  this.nuevousuario = {nombre: '', email: '', empresa: ''}
+
+})
+
+}
+
+}
+
+
+
